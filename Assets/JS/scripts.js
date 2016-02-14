b1c2e6aca8d671f254c66cad90f4c627d76afc9b
Tori Hedden <tori.hedden@gmail.com>
2016-02-14 12:22:03 -0500

    Aligns all elements into a more structured one-line format

--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -1,28 +1,13 @@
 //this targets the grid icon
-var formatClick = document.getElementById("grid-icon");
+var gridClick = document.getElementById("grid-icon");
 
 //this targets the list icon
-var formatClick2 = document.getElementById("list-icon");
-
+var listClick = document.getElementById("list-icon");
 
 //targets the list view stylesheet.
 var listStyleSheet = document.getElementById("list-view");
 // the grid view is the default style sheet.
 var gridStyleSheet = document.getElementById("default");
-
-//if you click the list icon, the grid style sheet is disabled.
-//the list view style sheet is un-disabled
-formatClick2.addEventListener("click", function (){
-    gridStyleSheet.disabled = true;
-    listStyleSheet.disabled = false;
-});
-
-//if you click the grid icon, the list style sheet disables
-//the grid style sheet will be un-disabled
-formatClick.addEventListener("click", function (){
-    gridStyleSheet.disabled = false;
-    listStyleSheet.disabled = true;
-});
 
 //creates variable for each class name string
 var gridBlue = "fa fa-th fa-2x blue";
@@ -30,14 +15,22 @@ var gridGray = "fa fa-th fa-2x gray";
 var listBlue = "fa fa-th-list fa-2x blue";
 var listGray = "fa fa-th-list fa-2x gray";
 
-//if you click the grid icon, turn it blue, and turn the list icon gray
-formatClick.addEventListener("click", function(){
-  formatClick.className = gridBlue;
-  formatClick2.className = listGray;
+//if you click the list icon, the grid style sheet is disabled.
+//the list view style sheet is un-disabled
+//if you click the list view, the icon turns blue. the grid icon will turn gray.
+listClick.addEventListener("click", function (){
+    gridStyleSheet.disabled = true;
+    listStyleSheet.disabled = false;
+    listClick.className = listBlue;
+    gridClick.className = gridGray;
 });
 
-//if you click the list icon, turn it blue, and turn the grid icon gray
-formatClick2.addEventListener("click", function(){
-  formatClick2.className = listBlue;
-  formatClick.className = gridGray;
+//if you click the grid icon, the list style sheet disables
+//the grid style sheet will be un-disabled
+//if you click the grid view, the icon turns blue. the list icon will turn gray.
+gridClick.addEventListener("click", function (){
+    gridStyleSheet.disabled = false;
+    listStyleSheet.disabled = true;
+    gridClick.className = gridBlue;
+    listClick.className = listGray;
 });
